6fcefb172b6908719a51ce91d203afb1d511b9b2
javid <javiddadashkarimi@gmail.com>
2018-09-07 12:10:18 -0400

    a

--- cpm.py
+++ cpm.py
@@ -198,18 +198,15 @@ def run_validate(X,y,cv_type):
 
 
 def kfold_cpm(ipmats,pheno,numsubs,k):
-  """
+    """
     Accepts input matrices and pheno data
     Returns model
     @author: David O'Connor
     @documentation: Javid Dadashkarimi
-    cpm: in cpm we select the most significant edges for subjects. so each subject
-         have a pair set of edges with positive and negative correlation with behavioral subjects.
-         It's important to keep both set in final regression task.  
-    posedges: positive edges are a set of edges have positive
-              correlatin with behavioral measures
-    negedges: negative edges are a set of edges have negative
-              correlation with behavioral measures
+    ipmats:
+    pheno:
+    numsubs:
+    k:
     """
 
 
