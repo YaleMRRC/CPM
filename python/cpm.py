d3ef157d90e8c6112be89f10b5cf0f7a46fc0dca
Javid Dadashkarimi <javid@Javids-MacBook-Pro.local>
2019-05-26 13:29:01 -0400

    add python pipeline

---
+++ python/cpm.py
@@ -0,0 +1,298 @@
+import numpy as np 
+import scipy as sp
+import pandas as pd
+#from matplotlib import pyplot as plt
+#import seaborn as sns
+import glob
+from scipy import stats
+import random
+import glob
+
+
+def generate_csv_list(path):
+    fn_list = glob.glob(path+'/*')
+    return fn_list
+
+def read_mats(fn_list):
+    """
+    Accepts list of csv file names where each csv contains a single subject FC matrix
+    Returns stacked matrices
+    """
+    fns = [pd.read_csv(fn, header=None) for fn in fn_list]
+    fns = [df.dropna(axis=1).values for df in fns]            
+    fn_mats = np.stack(fns, axis=2)
+    return fn_mats
+
+
+def train_cpm(fn_mats, pheno):
+
+    """
+    Accepts input matrices and pheno data
+    Returns model
+    @author: David O'Connor
+    @documentation: Javid Dadashkarimi
+    cpm: in cpm we select the most significant edges for subjects. so each subject
+         have a pair set of edges with positive and negative correlation with behavioral subjects.
+         It's important to keep both set in final regression task.  
+    posedges: positive edges are a set of edges have positive
+              correlatin with behavioral measures
+    negedges: negative edges are a set of edges have negative
+              correlation with behavioral measures
+    """
+
+    cc=[stats.pearsonr(pheno,im) for im in fn_mats]
+    rmat=np.array([c[0] for c in cc])
+    pmat=np.array([c[1] for c in cc])
+    rmat=np.reshape(rmat,[268,268])
+    pmat=np.reshape(pmat,[268,268])
+    posedges=(rmat > 0) & (pmat < 0.01)
+    posedges=posedges.astype(int)
+    negedges=(rmat < 0) & (pmat < 0.01)
+    negedges=negedges.astype(int)
+    pe=ipmat[posedges.flatten().astype(bool),:]
+    ne=ipmat[negedges.flatten().astype(bool),:]
+    pe=pe.sum(axis=0)/2
+    ne=ne.sum(axis=0)/2
+
+
+    if np.sum(pe) != 0:
+        fit_pos=np.polyfit(pe,pheno,1)
+    else:
+        fit_pos=[]
+
+    if np.sum(ne) != 0:
+        fit_neg=np.polyfit(ne,pheno,1)
+    else:
+        fit_neg=[]
+
+    return fit_pos,fit_neg,posedges,negedges
+
+
+def pairwise_corr(X,Y):
+    """
+    Accepts ...
+    Returns ...
+    """
+
+    #A=A.astype('float64')
+
+    n=X.shape[0]
+    
+    xbar=X.mean(1)
+    ybar=Y.mean(1)
+
+    xminusxbar=X-np.vstack(xbar)
+    yminusybar=Y-np.vstack(ybar)
+
+    xminusxbarsquare=xminusxbar**2
+    yminusybarsquare=yminusybar**2
+
+    numer=np.dot(xminusxbar.T,yminusybar)
+    denom=np.vstack(np.sqrt(xminusxbarsquare.sum(1))*np.sqrt(yminusybarsquare.sum(1)))
+
+    return numer/denom
+
+
+def run_validate(X,y,cv_type):
+    
+    
+    """
+    Accepts input matrices (X), phenotype data (y), and the type of cross-valdiation (cv_type)    
+    Returns the R-values for positive model (Rpos), negative model (Rneg), and the combination
+    X: the feature matrix of size (number of nodes x number of nodes x number of subjects)
+    y: the phenotype vector of size (number of subjects)
+    cv_type: the cross-valdiation type, takes one of the followings: 
+    1) LOO: leave-one-out cross-validation
+    2) 5k: 
+    """
+    num_subs=X.shape[2]
+    X=np.reshape(X,[-1,num_subs])
+
+    
+    if cvtype == 'LOO':
+        behav_pred_pos=np.zeros([numsubs])
+        behav_pred_neg=np.zeros([numsubs])
+        for loo in range(0,numsubs):
+
+            print("Running LOO, sub no:",loo)
+      
+            train_mats=np.delete(ipmats,[loo],axis=1)
+            train_pheno=np.delete(pheno,[loo],axis=0)
+            
+            test_mat=ipmats[:,loo]
+            test_pheno=pheno[loo]
+
+            pos_fit,neg_fit,posedges,negedges=train_cpm(train_mats,train_pheno)
+
+            pe=np.sum(test_mat[posedges.flatten().astype(bool)])/2
+            ne=np.sum(test_mat[negedges.flatten().astype(bool)])/2
+
+            if len(pos_fit) > 0:
+                behav_pred_pos[loo]=pos_fit[0]*pe + pos_fit[1]
+            else:
+                behav_pred_pos[loo]='nan'
+
+            if len(neg_fit) > 0:
+               behav_pred_neg[loo]=neg_fit[0]*ne + neg_fit[1]
+            else:
+                behav_pred_neg[loo]='nan'
+
+        
+        Rpos=stats.pearsonr(behav_pred_pos,pheno)[0]
+        Rneg=stats.pearsonr(behav_pred_neg,pheno)[0]
+
+        return Rpos,Rneg
+
+
+    elif cvtype == '5k':
+        bp,bn,ba=kfold_cpm(ipmats,pheno,numsubs,5)
+
+
+
+        ccp=np.array([stats.pearsonr(bp[i,:],ba[i,:]) for i in range(0,5)])
+        Rpos_mean=ccp.mean(axis=0)[0]
+
+        ccn=np.array([stats.pearsonr(bn[i,:],ba[i,:]) for i in range(0,5)])
+        Rneg_mean=ccn.mean(axis=0)[0]
+
+
+
+    elif cvtype == '10k':
+        bp,bn,ba=kfold_cpm(ipmats,pheno,numsubs,10)
+
+
+        ccp=np.array([stats.pearsonr(bp[i,:],ba[i,:]) for i in range(0,10)])
+        Rpos_mean=ccp.mean(axis=0)[0]
+
+        ccn=np.array([stats.pearsonr(bn[i,:],ba[i,:]) for i in range(0,10)])
+        Rneg_mean=ccn.mean(axis=0)[0]
+
+
+
+    elif cvtype == 'splithalf':
+        bp,bn,ba=kfold_cpm(ipmats,pheno,numsubs,2)
+
+        ccp=np.array([stats.pearsonr(bp[i,:],ba[i,:]) for i in range(0,2)])
+        Rpos_mean=ccp.mean(axis=0)[0]
+
+        ccn=np.array([stats.pearsonr(bn[i,:],ba[i,:]) for i in range(0,2)])
+        Rneg_mean=ccn.mean(axis=0)[0]
+
+
+    else:
+        raise Exception('cvtype must be LOO, 5k, 10k, or splithalf')
+
+
+    return Rpos_mean,Rneg_mean
+    
+
+
+def kfold_cpm(X,y,k):
+    """
+    Accepts input matrices and pheno data
+    Returns model
+    @author: David O'Connor
+    @documentation: Javid Dadashkarimi
+    X: is the input matrix in v*v*n which v is number of nodes and n is the number of subjects 
+    y: is the gold data which is fluid intelligence
+    k: is the size of folds in k-fold
+    """
+
+    numsubs = X.shape[2]
+    randinds=np.arange(0,numsubs)
+    random.shuffle(randinds)
+
+    samplesize=int(np.floor(float(numsubs)/k))
+
+    behav_pred_pos=np.zeros([k,samplesize])
+    behav_pred_neg=np.zeros([k,samplesize])
+
+    behav_actual=np.zeros([k,samplesize])
+
+    for fold in range(0,k):
+        print("Running fold:",fold+1)
+        si=fold*samplesize
+        fi=(fold+1)*samplesize
+
+
+        if fold != k-1:
+            testinds=randinds[si:fi]
+        else:
+            testinds=randinds[si:]
+
+        traininds=randinds[~np.isin(randinds,testinds)]
+        
+        trainmats=ipmats[:,traininds]
+        trainpheno=pheno[traininds]
+ 
+        testmats=ipmats[:,testinds]
+        testpheno=pheno[testinds]
+
+        behav_actual[fold,:]=testpheno
+
+
+        pos_fit,neg_fit,posedges,negedges=train_cpm(trainmats,trainpheno)
+
+        pe=np.sum(testmats[posedges.flatten().astype(bool),:], axis=0)/2
+        ne=np.sum(testmats[negedges.flatten().astype(bool),:], axis=0)/2
+
+
+        if len(pos_fit) > 0:
+            behav_pred_pos[fold,:]=pos_fit[0]*pe + pos_fit[1]
+        else:
+            behav_pred_pos[fold,:]='nan'
+
+        if len(neg_fit) > 0:
+            behav_pred_neg[fold,:]=neg_fit[0]*ne + neg_fit[1]
+        else:
+            behav_pred_neg[fold,:]='nan'
+
+    return behav_pred_pos,behav_pred_neg,behav_actual
+
+
+def sample_500(X,y,cv_type):
+    """
+    Accepts input matrices and pheno data
+    Returns 500 random samples from data
+    @author: David O'Connor
+    @documentation: Javid Dadashkarimi
+    X: is the input matrix in v*v*n which v is number of nodes and n is the number of subjects 
+    y: is the gold data which is fluid intelligence
+    k: is the size of folds in k-fold
+    """
+
+
+    numsubs=ipmats.shape[2]
+
+    randinds=np.arange(0,numsubs)
+    random.shuffle(randinds)
+
+    randinds500=randinds[:500]
+
+    ipmats_rand=ipmats[:,:,randinds500]
+    pheno_rand=pheno[randinds500]
+
+    opdict={}
+
+    Rpos_loo,Rneg_loo=run_validate(ipmats_rand,pheno_rand,'LOO')
+    
+    Rpos_2k,Rneg_2k=run_validate(ipmats_rand,pheno_rand,'splithalf')
+
+    Rpos_5k,Rneg_5k=run_validate(ipmats_rand,pheno_rand,'5k')
+
+    Rpos_10k,Rneg_10k=run_validate(ipmats_rand,pheno_rand,'10k')
+
+    opdict['LOO_Rpos'] = Rpos_loo
+    opdict['LOO_Rneg'] = Rneg_loo
+    opdict['2k_Rpos'] = Rpos_2k
+    opdict['2k_Rneg'] = Rneg_2k
+    opdict['5k_Rpos'] = Rpos_5k
+    opdict['5k_Rneg'] = Rneg_5k
+    opdict['10k_Rpos'] = Rpos_10k
+    opdict['10k_Rneg'] = Rneg_10k
+    opdict['Sample_Indices']=randinds500
+
+    return opdict
+
+
+
