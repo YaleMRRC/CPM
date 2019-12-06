788172ed448117dcd0c0de53f024400783dfd7e4
Javid Dadashkarimi <2877401+dadashkarimi@users.noreply.github.com>
2019-12-06 11:16:28 -0500

    Update rcpm.m

--- matlab/oop/rcpm.m
+++ matlab/oop/rcpm.m
@@ -16,8 +16,8 @@ classdef rcpm < predictory
                 fprintf('%dth fold\n', i_fold);
                 test.indx = indices.test(i_fold);
                 train.indx = indices.training(i_fold);
-                test.x = this.all_edges(:,test.indx);
-                train.x = this.all_edges(:,train.indx);
+                test.x = all_edges(:,test.indx);
+                train.x = all_edges(:,train.indx);
                 test.y = this.phenotype.all_behav(indices.test(i_fold),:);
                 train.y = this.phenotype.all_behav(indices.training(i_fold),:);
                 
